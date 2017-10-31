3e2d10cc5dcdd66dbe4cc4202c33f73ba19a7688
Guest User <Guest@Epicodus-6C.local>
2017-10-31 12:56:15 -0700

    add store-detail component

--- src/app/store-detail/store-detail.component.ts
+++ src/app/store-detail/store-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import {Store} from '../store.model';
+
 
 @Component({
   selector: 'app-store-detail',
@@ -6,10 +10,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./store-detail.component.css']
 })
 export class StoreDetailComponent implements OnInit {
-
-  constructor() { }
+  storeId: number = null;
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.storeId = parseInt(urlParameters['id']);
+    });
   }
 
 }
